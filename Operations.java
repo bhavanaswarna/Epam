6692663ce0c51d239a433c1a599b00290357b978
bhavanaswarna <47213598+bhavanaswarna@users.noreply.github.com>
2020-02-10 23:04:09 +0530

    Add files via upload

---
+++ Operations.java
@@ -0,0 +1,17 @@
+package week3;
+
+public class Operations {
+
+	double add(double x, double y) {
+		return x + y;
+	}
+	double subtract(double x, double y) {
+		return x - y;
+	}
+	double multiply(double x, double y) {
+		return x * y;
+	}
+	double divide(double x, double y) {
+		return x / y;
+	}
+}
